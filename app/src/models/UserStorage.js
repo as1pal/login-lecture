afaca5523f8cbb554332b58fa8cd96932a0f6d4d
s <s@wj.local>
2021-04-11 22:02:08 +0900

    백엔드에서 로그인 기능 구현

---
+++ app/src/models/UserStorage.js
@@ -0,0 +1,10 @@
+"use strict";
+
+class UserStorage {
+    users = {
+        id: ["IT", "개발", "김"],
+        password: ["123", "1231", "123124"],
+    };
+}
+
+module.exports = UserStorage;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,39 +1,38 @@
 "use strict";
 
-const output = {
-    hello: (req, res) => {
-        res.render("home/index")
-    },
-    login: (req, res) => {
-        res.render("home/login")
-    },
+const users = {
+    id: ["IT", "개발", "팀장"],
+    password: ["123", "1234", "123123"],
 };
 
-const users = {
-    id: ["IT", "개발", "김"],
-    password: ["123", "1231", "123124"],
+const output = {
+    home: (req, res) => {
+        res.render("home/index");
+    },
+    login: (req, res) => {
+        res.render("home/login");
+    },
 };
 
 const process = {
-    login(req, res) {
+    login: (req, res) => {
         const id = req.body.id,
             password = req.body.password;
-
+        
         if (users.id.includes(id)) {
             const idx = users.id.indexOf(id);
-
             if (users.password[idx] === password) {
                 return res.json({
                     success: true,
                 });
             }
         }
+
         return res.json({
             success: false,
-            msg: "로그인에 실패하셨습니다.",
-        }); 
+            msg: "로그인에 실패하셨습니다",
+        });
     },
-
 };
 
 module.exports = {
